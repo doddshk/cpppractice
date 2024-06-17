0e5a37408ca69e01dbcb3af843f83bdeed3925c1
cu-sp <down1995@tutanota.com>
2024-06-17 13:11:09 +0800

    bubble

--- bubblesort.cpp
+++ bubblesort.cpp
@@ -1,27 +1,7 @@
 #include <iostream>
-void bubble(std::vector<int>* vect);
+#include "bubble.h"
 
-int main()
-{
-  std::vector<int> vect;
-  vect.push_back(1);
-  vect.push_back(7);
-  vect.push_back(2);
-  vect.push_back(3);
-  for(const int n : vect)
-  {
-    std::cout<<n; 
-  }
-  std::cout<<"\n";
-  bubble(&vect);
-  for(const int n : vect)
-  {
-    std::cout<<n; 
-  }
-  return 0;
-}
-
-void bubble(std::vector<int>* vect)
+void bubble::bubble(std::vector<int>* vect)
 {
   for(int i = 0; i < vect->size() - 1; i++)
   {
