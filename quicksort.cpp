a503fd950ff433421153896da493dd68ac10601f
sw <sw@hanslaser.com>
2024-06-25 13:26:37 +0800

    Changes

--- quicksort.cpp
+++ quicksort.cpp
@@ -5,9 +5,17 @@ int quickSort(std::vector<int> arr, int high, int low)
 {
 	int low = 0; 
 	int high = 0;
+	std::vector<int> hvect;
+	std::vector<int> lvect;
 	for(int i = 0; i < piv; i++)
 	{
-		if(arr[i]>pivot
+		if(arr[i]>pivot)
+		{
+			hvect.push_back(arr[i]);
+		}
+
 	}
-  return 0;
+	std::vector<int> ans;
+	quicksort(hvect,0,0);
+  return quicksort(h;
 }
